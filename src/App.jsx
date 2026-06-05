b0711f65622398e93420355fd6ac06533e17406e
anshulms <anshuchauhan132004@gmail.com>
2026-06-05 20:34:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import {Button} from '../src/components/ui/button'
+import './App.css'
+import './index.css'
+import {NavbarMenu} from './modules/Navbar/NavbarMenu';
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div className="">
+        <NavbarMenu />
+        </div>
+     
+    </>
+  )
+}
+
+export default App
